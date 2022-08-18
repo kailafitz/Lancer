3338ec64263b514d88d9b908e0d83e14ca215d04
kailafitz <mikhailafitzpatrick@yahoo.com>
2022-08-18 12:30:14 +0100

    import structure change

--- app.py
+++ app.py
@@ -1,19 +1,5 @@
 from main import app
 from main.models import db
-import sys
-
-# if __name__ == '__main__':
-#     try:
-#         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-#         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#         db.app = app
-#         db.init_app(app)
-#         db.create_all()
-#         app.debug = True
-#         app.run()
-
-#     except KeyboardInterrupt:
-#         sys.exit(0)
 
 db.app = app
 db.init_app(app)
